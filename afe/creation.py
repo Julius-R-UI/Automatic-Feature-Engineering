0807c7e7bc95d05406d4ef485e6a29e8b9241248
jr-pixel <julius.riel@icloud.com>
2022-08-11 09:52:13 +0800

    .

---
+++ afe/creation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+class create:
+    def __init__(self, x, categorical_columns = []):
+        self.x = x
+        self.cat = categorical_columns
+    def add(self):
+        return 
