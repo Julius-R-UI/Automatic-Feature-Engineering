bd65ac2a4dfa44612c7cfe21f0a6279af048e538
Julius-R-UI <79837667+Julius-R-UI@users.noreply.github.com>
2023-05-24 14:35:03 +0800

    Updated
    Reorganised file structure

--- afe/__init__.py
+++
@@ -1,4 +0,0 @@
-#from .creation import simple
-from .creation import Simple as operations
-from .selection import Models as model
-from .metrics import metric as metrics
--- afe/metrics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class metric:
-    def R_Squared(y_true, y_pred):
-        SSE = np.sum((y_true - y_pred)**2)
-        SSW = np.sum((y_true - np.mean(y_true))**2)
-
-        return 1 - SSE/SSW
